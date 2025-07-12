975015c5ea9de9dc8486472771b5ad3518bac972
Gulshan <info@kamnatechno.com>
2025-07-12 09:20:30 +0530

    Fix: added www.nadi-astrology.com to allowedOrigins for CORS

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ dotenv.config();
 
 const app = express();
 
-// ✅ CORS - allow localhost:5173 and 5174
-const allowedOrigins = ["https://nadi-astrology.com"];
+
+const allowedOrigins = ["https://nadi-astrology.com","https://www.nadi-astrology.com"];
 
 app.use(
   cors({
